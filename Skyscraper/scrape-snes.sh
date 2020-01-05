8f9279534bed42f9705e1a5eac7b61a10a4f9084
Ozzy Smith <40549579+Sanic1239@users.noreply.github.com>
2020-01-04 18:41:37 -0800

    Add files via upload

---
+++ Skyscraper/scrape-snes.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# Replace the X's with your ScreenScraper Username:Password "-u xxxxx:xxxxx"
+
+./Skyscraper -s screenscraper -u xxxxx:xxxxx -t 2 --region us -p snes -m 70 --nomarquees --nocovers --nobrackets -i ~/recalbox/share/roms -g ~/recalbox/share/roms/snes -o ~/recalbox/share/roms/snes  --verbosity 1
