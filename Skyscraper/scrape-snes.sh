b5d9aaa7952d42e4dbc2b0d144f0e99c969c8423
Ozzy Smith <40549579+Sanic1239@users.noreply.github.com>
2020-01-04 18:51:56 -0800

    Update scrape-snes.sh

--- Skyscraper/scrape-snes.sh
+++ Skyscraper/scrape-snes.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 # Replace the X's with your ScreenScraper Username:Password "-u xxxxx:xxxxx"
+#Delete from "-u xxxxx:xxxxx" to "-t 2" if you don't have a ScreenScraper account.
 
 ./Skyscraper -s screenscraper -u xxxxx:xxxxx -t 2 --region us -p snes -m 70 --nomarquees --nocovers --nobrackets -i ~/recalbox/share/roms -g ~/recalbox/share/roms/snes -o ~/recalbox/share/roms/snes  --verbosity 1
